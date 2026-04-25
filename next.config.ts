c45b294928207536f4d8c4d9e2257ea20698f708
Afnan Sayed <afnansayed1973@gmail.com>
2026-04-25 11:25:05 +0600

    deplyoment

--- next.config.ts
+++ next.config.ts
@@ -13,7 +13,17 @@ const nextConfig: NextConfig = {
     serverActions: {
       bodySizeLimit: '5mb', // or '10mb'
     },
-  }
+  },
+
+  // rewrites() {
+  //   return [
+  //    {
+  //       // Explicitly map v1 API requests
+  //       source: "/api/v1/:path*",
+  //       destination: process.env.NEXT_PUBLIC_API_BASE_URL + "/api/v1/:path*",
+  //     },
+  //   ];
+  // }
 
 };
 
